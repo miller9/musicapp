52d395fbb04b4c6f9a167f1d4a8bcb8f05a17e67
miller <millercarreno@seven4n.com>
2018-09-16 07:36:08 -0500

    Adding 'index' method to save all the playlists

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,7 @@
 class PlaylistsController < ApplicationController
 	def index
+		# Save in var all the playlists, then use it in the View to show the info
+		# @ ==> instance var access to Playlist MODEL and gets all of the lists
+		@playlists = Playlist.all
 	end
 end
