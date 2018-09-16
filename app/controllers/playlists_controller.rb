047eb9db5af0f7c90d1e23ce7616ba0e64797ccb
miller <millercarreno@seven4n.com>
2018-09-16 07:56:42 -0500

    Creating Show's Method in the playlist controller

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,7 +1,13 @@
 class PlaylistsController < ApplicationController
+	# Save in var all the playlists, then use it in the View to show the info
+	# @ ==> instance var access to Playlist MODEL and gets all of the lists
 	def index
-		# Save in var all the playlists, then use it in the View to show the info
-		# @ ==> instance var access to Playlist MODEL and gets all of the lists
 		@playlists = Playlist.all
 	end
+
+	# Save the playlists to show every one we want using the respective id
+	def show
+		@playlist = Playlist.find(params[:id])
+	end
+
 end
