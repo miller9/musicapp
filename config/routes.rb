20194f8dabe5c5184c50ec8008b3b0d545555033
miller <millercarreno@seven4n.com>
2018-09-18 19:40:02 -0500

    Adding default root and generating devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-root to: 'tracks#index'
+  devise_for :users
   get 'spotify/search'
+  
   resources :tracks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :playlists # Adding new 7 standard routes
+
+	root to: 'tracks#index'
+
+
 end
