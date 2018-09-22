7f6bba42a3cd08bb661fdfdf0edfd751bced18ec
miller <millercarreno@seven4n.com>
2018-09-21 19:51:08 -0500

    Adding 'search by song'

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -2,16 +2,24 @@
 # require 'rspotify'
 class SpotifyController < ApplicationController
   def search
-  	# binding.pry
-  	# puts @results.inspect
+
   	# Extract parameter and save it
   	title = params[:title]
   	# validating the field 'title' isn't empty
   	if title
-  		# puts @results.inspect
   		@results = RSpotify::Artist.search(title)
+      @songs = RSpotify::Track.search(title)
+
+      #@artist = search.first
   	end
   end
+
+  # def search2
+  #   title = params[:title]
+  #   if title
+  #     @songs = RSpotify::Track.search(title)
+  #   end
+  # end
 
   # private
   # def spotify_params
